a8c5865c8ba8338639c3d89bb98ae46d3ed2074a
beradug22 <beradug22@gmail.com>
2024-10-16 00:05:03 +0530

    Updated footer

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,27 +1,10 @@
-// "use client";
-// import Navbar from "./Navbar";
-// import Footer from "./Footer";
-
-// const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-//   return (
-//     <div className="flex flex-col min-h-[100dvh]">
-
-//       <Navbar />
-//       <main className="flex-1">{children}</main>
-//       <Footer />
-//     </div>
-//   );
-// };
-
-// export default Layout;
-
 "use client";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col  min-h-[100dvh]">
       <Navbar />
       <main className="flex-1">{children}</main>
       <Footer />
